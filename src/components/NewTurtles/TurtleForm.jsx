ea4c43b4b771246db240850fe638b127ef8645d3
Mazen <mazenadel19@gmail.com>
2021-06-29 16:49:33 +0200

    using ref instead of state

--- src/components/NewTurtles/TurtleForm.jsx
+++ src/components/NewTurtles/TurtleForm.jsx
@@ -1,46 +1,46 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Button from '../UI/Button/Button';
 import Card from '../UI/Card/Card';
 import ErrorModal from '../UI/Modal/ErrorModal';
 import styles from './TurtleForm.module.css';
 
 const TurtleForm = ({ onAddTurtle }) => {
-	const [name, setName] = useState('');
-	const [age, setAge] = useState('');
+	const nameInputRef = useRef();
+	const ageInputRef = useRef();
+
 	const [error, setError] = useState();
-
-	const nameChangeHandler = (e) => {
-		setName(e.target.value);
-	};
-
-	const ageChangeHandler = (e) => {
-		setAge(e.target.value);
-	};
 
 	const submitHandler = (e) => {
 		e.preventDefault();
-		if (name.trim().length === 0)
+
+		const turtleName = nameInputRef.current.value;
+		const turtleAge = ageInputRef.current.value;
+
+		if (turtleName.trim().length === 0) {
 			return setError({
 				title: 'Invalid Name',
 				message: "Turtle name can't be empty",
 			});
-		if (age.trim().length === 0)
+		}
+		if (turtleAge.trim().length === 0) {
 			return setError({
 				title: 'Invalid Age',
 				message: "Turtle age can't be empty",
 			});
-		if (+age < 0)
+		}
+		if (+turtleAge < 0) {
 			return setError({
 				title: 'Invalid Age',
 				message: "RIP turtle...Turtle age can't be a negative number",
 			});
+		}
 
 		onAddTurtle({
-			name,
-			age,
+			name: turtleName,
+			age: turtleAge,
 		});
-		setName('');
-		setAge('');
+		nameInputRef.current.value = '';
+		ageInputRef.current.value = '';
 	};
 
 	const errorHandler = () => {
@@ -60,27 +60,11 @@ const TurtleForm = ({ onAddTurtle }) => {
 				<form method='post' className={styles.form} onSubmit={submitHandler}>
 					<div>
 						<label htmlFor='name'>Turtle Name</label>
-						<input
-							type='text'
-							name='name'
-							id='name'
-							value={name}
-							onChange={nameChangeHandler}
-							className={`${!name && styles['invalid-input']}`}
-						/>
+						<input type='text' name='name' id='name' ref={nameInputRef} />
 					</div>
 					<div>
 						<label htmlFor='age'>Turtle Age (Years)</label>
-						<input
-							type='number'
-							name='age'
-							id='age'
-							value={age}
-							onChange={ageChangeHandler}
-							className={`${!age && styles['invalid-input']} ${
-								age < 0 && styles['invalid-input']
-							}`}
-						/>
+						<input type='number' name='age' id='age' ref={ageInputRef} />
 					</div>
 					<Button type='submit'>Add Turtle</Button>
 				</form>
--- src/components/UI/Card/Card.jsx
+++ src/components/UI/Card/Card.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './Card.module.css';
 
 const Card = (props) => {
-	console.log(props);
 	return (
 		<div className={`${styles.turtleform} ${props.className}`}>
 			{props.children}
